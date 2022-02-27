81ca2dc7e868a0ac31962fcbbb71f8ac673b9fe6
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-02-27 22:48:37 +0700

    success to create, delete,and get user . coding is finish!

--- controllers/user.go
+++ controllers/user.go
@@ -41,3 +41,37 @@ func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httpr
 	w.WriteHeader(http.StatusOK) // 200
 	fmt.Fprintf(w, "%s\n", uj)
 }
+
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	u := models.User{}
+
+	json.NewDecoder(r.Body).Decode(&u)
+
+	u.Id = bson.NewObjectId()
+	uc.session.DB("project-mongodb").C("users").Insert(u)
+
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(404)
+		return
+	}
+	oid := bson.ObjectIdHex(id)
+
+	if err := uc.session.DB("project-mongodb").C("users").RemoveId(oid); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Deleted user", oid, "\n")
+}
--- models/user.go
+++ models/user.go
@@ -3,8 +3,11 @@ package models
 import "gopkg.in/mgo.v2/bson"
 
 type User struct {
-	IdB    bson.ObjectId `bson:"_id" json:"id"`
-	Name   string        `json:"name" bson:"name"`
-	Gender string        `json:"gender bson:"gender"`
-	Age    int           `json:"age" bson:"age"`
+	Id          bson.ObjectId `bson:"_id" json:"id"`
+	Name        string        `json:"name" bson:"name"`
+	Gender      string        `json:"gender" bson:"gender"`
+	Age         int           `json:"age" bson:"age"`
+	Address     string        `json:"address" bson:"address"`
+	Nationality string        `json:"nationality" bson:"nationality"`
+	Hobby       string        `json:"hobby" bson:"hobby"`
 }
